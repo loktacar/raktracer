9521a9598148bbea798ef26234983bc8a0675886
Viktor Alex <viktoralex@365.is>
2016-03-30 17:43:47 +0000

    Bad support for multiple spheres

--- raktracer.go
+++ raktracer.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"github.com/loktacar/raktracer/lib"
 	"image"
 	"image/color"
@@ -17,37 +17,55 @@ var black = color.RGBA{0, 0, 0, 255}
 var diffuseCoefficient = 0.95
 
 func main() {
-	s := raktracer.Sphere{raktracer.Vector{0, 0, 50}, 100}
+	spheres := []raktracer.Sphere{
+		raktracer.Sphere{raktracer.Vector{75, 0, 550}, 100},
+		raktracer.Sphere{raktracer.Vector{-75, 0, 450}, 100},
+	}
 	light := raktracer.Vector{256, 512, -500}
 	//light := raktracer.Vector{0, 0, -1000}
+
+	for x := 0; x < 512; x++ {
+		for y := 0; y < 512; y++ {
+			img.Set(x, y, black)
+		}
+	}
 
 	for x := -256; x < 256; x++ {
 		for y := -256; y < 256; y++ {
 			camPos := raktracer.Vector{float64(x), float64(y), 0}
 
 			r := raktracer.Ray{camPos, raktracer.Vector{0, 0, 1}}
-			i, dist1, _ := s.Intersects(r)
-			if i {
-				intersect := camPos.Add(r.Dir.Scale(dist1))
+			for _, s := range spheres {
+				i, dist := s.Intersects(r)
+				if i {
+					intersect := camPos.Add(r.Dir.Scale(dist))
 
-				lV := light.Subtract(intersect).Normalize()
-				n := s.Pos.Subtract(intersect).Normalize()
-				if n.Z > 0 {
-					//fmt.Printf("Negative n.Z\n")
-					fmt.Printf("Positive n.Z\n")
+					lV := light.Subtract(intersect).Normalize()
+
+					lightIntersection := false
+					lR := raktracer.Ray{intersect, lV}
+					for _, s2 := range spheres {
+						if s2 != s {
+							iL, _ := s2.Intersects(lR)
+							lightIntersection = iL || lightIntersection
+						}
+					}
+
+					diffLightValue := 0.05
+					if !lightIntersection {
+						n := s.Pos.Subtract(intersect).Normalize()
+
+						diffLightValue += diffuseCoefficient * math.Max(0, n.Dot(lV)) * 0.95
+					}
+					//fmt.Printf("%s . %s = %.2f\n", lV, n, diffLightValue)
+					c := color.RGBA{
+						uint8(255 * diffLightValue),
+						uint8(255 * diffLightValue),
+						uint8(255 * diffLightValue),
+						255}
+
+					img.Set(x+255, y+255, c)
 				}
-
-				diffLightValue := 0.1 + (diffuseCoefficient * math.Max(0, n.Dot(lV)) * 0.9)
-				//fmt.Printf("%s . %s = %.2f\n", lV, n, diffLightValue)
-				c := color.RGBA{
-					uint8(255 * diffLightValue),
-					uint8(255 * diffLightValue),
-					uint8(255 * diffLightValue),
-					255}
-
-				img.Set(x+255, y+255, c)
-			} else {
-				img.Set(x+255, y+255, black)
 			}
 		}
 	}
